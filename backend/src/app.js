6c885ce6a6bb22fa44b4c1d0ab3fca8d100e4b0a
ctlcp2000 <charlieadamsyes@gmail.com>
2025-09-05 13:30:34 +0000

    feat: finalize initial backend implementation

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,50 @@
 import express from 'express'
+import {
+  postsRoutes,
+  createPost,
+  updatePost,
+  deletePost,
+} from './routes/posts.js'
+import bodyParser from 'body-parser'
+import cors from 'cors'
 
 const app = express()
+app.use(cors())
+app.use(bodyParser.json())
+postsRoutes(app)
+
 app.get('/', (req, res) => {
   res.send('Hello from Express!')
+})
+
+app.post('/api/v1/posts', async (req, res) => {
+  try {
+    const post = await createPost(req.body)
+    return res.json(post)
+  } catch (err) {
+    console.error('error creating post', err)
+    return res.status(500).end()
+  }
+})
+
+app.patch('/api/v1/posts/:id', async (req, res) => {
+  try {
+    const post = await updatePost(req.params.id, req.body)
+    return res.json(post)
+  } catch (err) {
+    console.error('error updating post', err)
+    return res.status(500).end()
+  }
+})
+
+app.delete('/api/v1/posts/:id', async (req, res) => {
+  try {
+    const { deletedCount } = await deletePost(req.params.id)
+    if (deletedCount === 0) return res.sendStatus(404)
+    return res.status(204).end()
+  } catch (err) {
+    console.error('error deleting post', err)
+    return res.status(500).end()
+  }
 })
 export { app }
