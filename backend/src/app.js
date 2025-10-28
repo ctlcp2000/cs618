a18cff36b12cc5529d9833b458e7884d85448cba
ctlcp2000 <charlieadamsyes@gmail.com>
2025-10-28 21:50:48 +0000

    feat: add events tracking

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,12 +4,14 @@ import { postsRoutes } from './routes/posts.js'
 import { userRoutes } from './routes/users.js'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import { eventRoutes } from './routes/events.js'
 
 const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 postsRoutes(app)
 userRoutes(app)
+eventRoutes(app)
 
 app.get('/', (req, res) => {
   res.send('Hello from Express!')
